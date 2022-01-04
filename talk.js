f75bcff0966f8bc8d09a3636a40fbf3ea5481d29
YellowHan <89592727+YellowHan@users.noreply.github.com>
2022-01-04 14:18:12 +0900

    Add files via upload

--- talk.js
+++ talk.js
@@ -1,61 +1,83 @@
 let inMsg;
 
-document.getElementById("output").innerText = "통통 튀는 매력의 라봉이와 대화 해봐요-!!";
+document.getElementById("output").innerText =
+  "통통 튀는 매력의 라봉이와 대화 해봐요-!!";
 
 document.addEventListener("click", (event) => {
   const target = event.target;
   if (target.id == "send") {
     inMsg = document.getElementById("user_msg").value;
     check();
-    inner();
     document.getElementById("user_msg").value = "";
+  }
+  if (target.id == "help") {
+    if(document.getElementById('pop').style.display == "none") {
+      document.getElementById('pop').style.display = "inline";
+    }
+    else {
+      document.getElementById('pop').style.display = "none";
+    }
   }
 });
 
 const check = () => {
-  if(inMsg.includes('안녕')) {
+  if (inMsg.includes("안녕")) {
     document.getElementById("output").innerText = "안녕하세요오옹~";
-  }
-  if(inMsg.includes('이름')) {
+  } else if (inMsg.includes("이름")) {
     document.getElementById("output").innerText = "라봉이에요오옹~";
-  }
-  if(inMsg.includes('불') && inMsg.includes('꺼') || inMsg.includes('끄')) {
-    let random = Math.floor(Math.random()*3)+1;
-    switch(random) {
-      case 1 : document.getElementById("output").innerText = "네? 뭐라고 하셨어요?";
-      break;
-      case 2 : document.getElementById("output").innerText = "??????????";
-      break;
-      case 3 : document.getElementById("output").innerText = "넵!!";
-      document.body.style.background = "black";
-      document.getElementById('user_msg').style.color = "white";
-      break;
+  } else if (
+    (inMsg.includes("불") && inMsg.includes("꺼")) || inMsg.includes("끄")) {
+    let random = Math.floor(Math.random() * 3) + 1;
+    switch (random) {
+      case 1:
+        document.getElementById("output").innerText = "네? 뭐라고 하셨어요?";
+        break;
+      case 2:
+        document.getElementById("output").innerText = "??????????";
+        break;
+      case 3:
+        document.getElementById("output").innerText = "넵!!";
+        document.body.style.background = "black";
+        document.getElementById("user_msg").style.color = "white";
+        break;
     }
-  }
-  if(inMsg.includes('불') && inMsg.includes('켜')) {
+  } else if (inMsg.includes("불") && inMsg.includes("켜")) {
     document.getElementById("output").innerText = "불 켜져랏~ 얍-!!";
-    document.getElementById("bot_img").style.background = "url(./img/lightOn.png) no-repeat";
+    document.getElementById("bot_img").style.background =
+      "url(./img/lightOn.png) no-repeat";
     document.getElementById("bot_img").style.backgroundSize = "contain";
     setTimeout(() => {
       document.body.style.background = "white";
-      document.getElementById("bot_img").style.background = "url(./img/lightOnDone.png) no-repeat";
+      document.getElementById("bot_img").style.background =
+        "url(./img/lightOnDone.png) no-repeat";
       document.getElementById("bot_img").style.backgroundSize = "contain";
-      document.getElementById('user_msg').style.color = "black";
-    },1000);
+      document.getElementById("user_msg").style.color = "black";
+    }, 1000);
+  } else if (inMsg.includes("바보")) {
+    document.getElementById("output").innerText = "파.업.";
+    document.getElementById("bot_img").style.background =
+      "url(./img/iwont.png) no-repeat";
+    document.getElementById("bot_img").style.backgroundSize = "contain";
+    setTimeout(() => {
+      document.getElementById("bot_img").style.background =
+        "url(./img/iwontDone.png) no-repeat";
+      document.getElementById("bot_img").style.backgroundSize = "contain";
+    }, 400);
+  } else if (inMsg.includes("미안")) {
+    document.getElementById("output").innerText = "괜차나요옹~";
+    document.getElementById("bot_img").style.background =
+      "url(./img/default.png) no-repeat";
+    document.getElementById("bot_img").style.backgroundSize = "contain";
+  } else if (inMsg == "고마워") {
+    document.getElementById("output").innerText =
+      "고마움의 표현은 입금으로 ♥_♥";
+    document.getElementById("bot_img").style.background =
+      "url(./img/twinkle.png) no-repeat";
+    document.getElementById("bot_img").style.backgroundSize = "contain";
+  } else {
+    document.getElementById("output").innerText = "잘 모르겠어요..";
+    document.getElementById("bot_img").style.background =
+      "url(./img/what.png) no-repeat";
+    document.getElementById("bot_img").style.backgroundSize = "contain";
   }
 };
-
-const inner = () => {
-  if(inMsg.includes('안녕')) {
-    console.log('어, 그래~');
-  }
-  if(inMsg.includes('이름')) {
-    console.log('알아서 뭐하게?');
-  }
-  if(inMsg.includes('불') && inMsg.includes('꺼')) {
-    console.log('아, 귀찮게 하네');
-  }
-  if(inMsg.includes('불') && inMsg.includes('켜')) {
-    console.log('으휴.. 니가 켜라!');
-  }
-}
